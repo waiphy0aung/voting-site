e803baacc0f0b5375960366722d44b434e3342d8
waiphy0aung <bgyi354@gmail.com>
2022-08-05 07:42:38 +0630

    update

---
+++ resources/js/components/dashboard/VoteCount.vue
@@ -0,0 +1,14 @@
+<template>
+    <Master>
+        
+    </Master>
+</template>
+<script>
+import Master from "./layouts/Master.vue";
+export default {
+    components : {Master},
+    computed : {
+        
+    }
+}
+</script>
